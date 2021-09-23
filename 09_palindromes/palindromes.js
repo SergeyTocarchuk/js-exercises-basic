e5bd477c250ca44f73af1055e263472005b3fcc7
Serhii T <tocarchuk@gmail.com>
2021-09-23 12:06:36 +0300

    finish exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let firstStr = str.toLowerCase().replace(/[^A-Za-z]/g, "");
+  let secondStr = '';
+  let tempStr = firstStr.split(''); 
+  for( let i = firstStr.length - 1; i >= 0; i-- ){
+    secondStr += tempStr[i];
+  }
+  if( firstStr === secondStr ){
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  if( index < 0 ) return 'OOPS';
+  if( index < 2 ) return 1;
 
+  let arr = [1, 1];
+  for( let i = 0; i < index - 2; i++ ){
+      let tempNum = arr[i] + arr[i + 1];
+      arr.push(tempNum);
+      console.log(arr);
+  }
+  return arr[index - 1];
 };
 
 // Do not edit below this line
